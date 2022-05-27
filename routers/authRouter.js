a7060adcf2d2fe8d26c7a008224b37c3509499cb
thuan2404 <nguyeminhthuan24420@gmail.com>
2022-05-27 12:01:09 +0700

    update

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -2,9 +2,9 @@ import express from "express";
 
 const authRouter = express.Router();
 authRouter.get("/login", (request, response) => {
-  response.render("login");
+  response.render("login/login");
 });
 authRouter.get("/signup", (request, response) => {
-  response.render("signup");
+  response.render("signup/signup");
 });
 export default authRouter;
---
+++ views/photos/viewPhoto.js
@@ -0,0 +1,17 @@
+function closePhoto(){
+    var b = document.getElementsByClassName("show-photo")[0];
+    b.classList.remove("show-photo-active");
+}
+function showPhoto(img, tt, ct){
+    var height = document.getElementsByClassName("wrapper")[0];
+    var photoShow = document.getElementsByClassName("show-photo")[0];
+    var image = document.getElementsByClassName("image-show")[0];
+    var title = document.getElementsByClassName("photo-title-image")[0];
+    var content = document.getElementsByClassName("photo-content-image")[0];
+    photoShow.classList.add("show-photo-active");
+    photoShow.style.height = (height.clientHeight+100) + 'px'
+    image.src = img;
+    title.innerHTML = tt;
+    content.innerHTML = ct;
+}
+
