2f6d4998c3b3e6414154dd9f9a08b42a0ca0184d
thuan2404 <nguyeminhthuan24420@gmail.com>
2022-06-07 13:26:52 +0700

    update

--- config/Passport.js
+++ config/Passport.js
@@ -21,29 +21,25 @@ passport.use(
       passwordField: "password",
       passReqToCallback: true,
     },
-    (req, email, password, done) => {
-      UserModel.findOne({ email: email }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
+    async (req, email, password, done) => {
+      try {
+        const user = await UserModel.findOne({ email: email });
         if (user) {
           return done(null, false, { message: "Email is already in use!" });
         }
         if (password !== req.body.confirmPassword) {
           return done(null, false, { message: "Confirm Password not match!" });
         }
-        var newUser = new UserModel();
+        const newUser = new UserModel();
         newUser.firstName = req.body.firstName;
         newUser.lastName = req.body.lastName;
         newUser.email = email;
         newUser.password = newUser.encryptPassword(password);
-        newUser.save(function (err, result) {
-          if (err) {
-            return done(err);
-          }
-          return done(null, newUser);
-        });
-      });
+        await newUser.save();
+        return done(null, newUser);
+      } catch (error) {
+        return done(error);
+      }
     }
   )
 );
@@ -56,19 +52,19 @@ passport.use(
       passwordField: "password",
       passReqToCallback: true,
     },
-    (req, email, password, done) => {
-      UserModel.findOne({ email: email }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
+    async (req, email, password, done) => {
+      try {
+        const user = await UserModel.findOne({ email: email });
         if (!user) {
-          return done(null, false, { message: "Not user found" });
+          return done(null, false, { message: "Sign in unsuccessful!" });
         }
         if (!user.validPassword(password)) {
-          return done(null, false, { message: "Wrong password" });
+          return done(null, false, { message: "Sign in unsuccessful!" });
         }
         return done(null, user);
-      });
+      } catch (error) {
+        return done(error);
+      }
     }
   )
 );
